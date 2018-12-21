bf7f9ac49729655e1d293275ef05d65cde20ddf3
Srihari Subramanian <sri.sub000@gmail.com>
2018-12-20 20:59:18 -0500

    Elevator positions

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,6 +5,13 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Shooter extends Subsystem{
     private DoubleSolenoid shooter, highElevator, lowElevator;
+
+    public enum ElevatorPosition {
+        DOWN,
+        LOW,
+        MEDIUM,
+        HIGH;
+    }
     
     //PCM ID's
     private final int PCM_ID = 6, 
@@ -14,7 +21,9 @@ public class Shooter extends Subsystem{
                       HIGH_ELEVATOR_REVERSE = 2,
                       LOW_ELEVATOR_FORWARD = 4,
                       LOW_ELEVATOR_REVERSE = 3;
-                    
+    //variable currently not in use
+    private ElevatorPosition currPos = ElevatorPosition.LOW;
+
     public Shooter() {
         shooter = new DoubleSolenoid(PCM_ID, SHOOTER_FORWARD, SHOOTER_REVERSE);
         highElevator = new DoubleSolenoid(PCM_ID, HIGH_ELEVATOR_FORWARD, HIGH_ELEVATOR_REVERSE);
@@ -22,7 +31,28 @@ public class Shooter extends Subsystem{
     }
 
     public void initDefaultCommand() {
+
+    }
+
+    public void setShooterHeight(ElevatorPosition pos) {
+		this.currPos = pos;       
+        switch(pos) {
+            case DOWN:			
+                lowElevator.set(DoubleSolenoid.Value.kReverse);
+                highElevator.set(DoubleSolenoid.Value.kReverse);
+                break;
+            case LOW:
+                lowElevator.set(DoubleSolenoid.Value.kForward);
+                highElevator.set(DoubleSolenoid.Value.kReverse);
+                break;
+            case MEDIUM:
+                lowElevator.set(DoubleSolenoid.Value.kReverse);
+                highElevator.set(DoubleSolenoid.Value.kForward);
+                break;
+            case HIGH:
+                highElevator.set(DoubleSolenoid.Value.kForward);
+                lowElevator.set(DoubleSolenoid.Value.kForward);
+                break;   
         
     }
 }
-
